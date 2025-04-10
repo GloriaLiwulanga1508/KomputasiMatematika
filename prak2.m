1593e0fdc467dcaed5a1f07a90209e79c7fb5eb6
unknown <liwulangaglo@gmail.com>
2025-04-10 10:59:19 +0800

    GloTry

---
+++ prak2.m
@@ -0,0 +1,17 @@
+clc
+clear
+
+ %Menghitung luas persegi panjang
+ p =10;
+ l =20;
+ lp = p*l;
+ disp(lp)
+ % disp(lp) untuk menampilkan hasil operasi 
+ 
+ lp1 = p+1;
+ lp2 = p-1;
+ lp3 = p/1;
+ 
+ disp(lp1)
+ disp(lp2)
+ disp(lp3)
